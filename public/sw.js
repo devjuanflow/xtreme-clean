8fb2caf277c6624e5a7d7c733350498cb654c237
devjuanflow <juancarlosortizgomez63@gmail.com>
2026-09-03 21:03:23 -0500

    Actualizar header completo para PWA

--- public/sw.js
+++ public/sw.js
@@ -1,6 +1,6 @@
 self.addEventListener('install', (event) => {
     event.waitUntil(
-        caches.open('xtreme-clean-v1').then((cache) => {
+        caches.open('xtreme-clean-v2').then((cache) => { // Cambiado de v1 a v2
         return cache.addAll([
             '/',
             '/admin/login',
@@ -10,10 +10,24 @@ self.addEventListener('install', (event) => {
     );
     });
 
+    self.addEventListener('activate', (event) => {
+    event.waitUntil(
+        caches.keys().then((cacheNames) => {
+        return Promise.all(
+            cacheNames.map((cacheName) => {
+            if (cacheName !== 'xtreme-clean-v2') {
+                return caches.delete(cacheName); // Borra la caché vieja
+            }
+            })
+        );
+        })
+    );
+    });
+
     self.addEventListener('fetch', (event) => {
     event.respondWith(
-        caches.match(event.request).then((response) => {
-        return response || fetch(event.request);
+        caches.match(event.request).then((cachedResponse) => {
+        return cachedResponse || fetch(event.request);
         })
     );
 });
